6feab556686c48158fbc98c488fb0657bbba4f5c
sudhi7seas <87425107+sudhi7seas@users.noreply.github.com>
2026-06-11 11:16:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'dannie-memorial-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
